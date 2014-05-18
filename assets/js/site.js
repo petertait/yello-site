438e2466fcc4ded6c3654dbc81dbaead37fa63f8
petertait <contact@petertait.com>
2014-05-18 13:33:34 +0100

    Updated Slider

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,3 +1,12 @@
 $(function() {
-
+	
+	// Features Slider
+	$('.features-slider').slick({
+	    dots: true,
+	    infinite: true,
+	    speed: 300,
+	    slidesToShow: 1,
+	    slidesToScroll: 1
+	});
+	
 });	
