5865a27d5257c6b0adeedb64fa87c7445ddb160e
petertait <contact@petertait.com>
2014-05-19 00:18:51 +0100

    Updated Map Style

--- assets/js/site.js
+++ assets/js/site.js
@@ -4,7 +4,8 @@ $(function() {
 	$('.features-slider').slick({
 	    dots: true,
 	    infinite: true,
-	    speed: 300,
+	    speed: 500,
+	    easing: 'ease-in-out',
 	    slidesToShow: 1,
 	    slidesToScroll: 1
 	});
